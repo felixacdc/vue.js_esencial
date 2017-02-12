79e83348f4b65edabe90ee1d28416c088627058e
felixacdc <pfmata01@gmail.com>
2017-02-11 23:09:05 -0600

    [add] Aprende Vue.js esencial #5 - Renderizado de listas

---
+++ 05/main.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: 'body',
+    data: {
+        // Objeto
+        persona: {
+            nombre: "Juan",
+            profesion: "Dev",
+            ciudad: "Valencia"
+        },
+
+        // Matriz simple
+        numeros: [
+            1,2,3,4,5
+        ]
+    }
+});
