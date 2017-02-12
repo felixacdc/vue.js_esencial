3c49334945e95636c1560e3ac374bb9d47bd6619
felixacdc <pfmata01@gmail.com>
2017-02-11 23:47:22 -0600

    [modify] Aprende Vue.js esencial #5 - Renderizado de listas

--- 05/main.js
+++ 05/main.js
@@ -11,6 +11,17 @@ new Vue({
         // Matriz simple
         numeros: [
             1,2,3,4,5
+        ],
+
+        // Matrices de objetos
+        semana: [
+            {nombre: "Lunes", numero: 1},
+            {nombre: "Martes", numero: 2},
+            {nombre: "Miercoles", numero: 3},
+            {nombre: "Jueves", numero: 4},
+            {nombre: "Viernes", numero: 5},
+            {nombre: "Sabado", numero: 6},
+            {nombre: "Domingo", numero: 7}
         ]
     }
 });
