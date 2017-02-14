0e17b17918fa2f65b2aa67058428fe52b6a431a7
felixacdc <pfmata01@gmail.com>
2017-02-13 22:39:30 -0600

    [add] Aprende Vue.js esencial #6 - Data binding en estilos y clases

---
+++ 06/main.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: "body",
+    data: {
+        azul: true,
+        subrayado: false,
+        objetoConClases: {
+            'azul': true,
+            'subrayado': true
+        }
+    }
+});
