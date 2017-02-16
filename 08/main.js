aaf95cad713af8f97b5240f96b7785f0f385284f
felixacdc <pfmata01@gmail.com>
2017-02-15 22:02:38 -0600

    [add] Aprende Vue.js esencial #8 - Introducción a componentes

---
+++ 08/main.js
@@ -0,0 +1,22 @@
+new Vue({
+    el: 'body',
+    ready: function() {
+        this.elegirCita();
+    },
+    data: {
+        citaElegida: '',
+        citas: [
+            '"It might help if we ran the MBAs out of Washington." -- Admiral Grace Hopper',
+            '"Whether you think you can or you think you can’t, you’re right." -- Henry Ford',
+            '"Every child is an artist.  The problem is how to remain an artist once he grows up." -- Pablo Picassor',
+            '""If you can dream it, you can do it" -- Walt Disney',
+            '"Genius is one percent inspiration, ninety nine percent perspiration" -- Thomas Edison'
+        ]
+    },
+    methods: {
+        elegirCita: function() {
+            let cita = this.citas[Math.floor(Math.random() * this.citas.length)];
+            this.citaElegida = cita;
+        }
+    }
+});
