331d02cb11a047a1955823f160dff44b80ff33e0
felixacdc <pfmata01@gmail.com>
2017-02-16 22:25:43 -0600

    [modify] Aprende Vue.js esencial #9 - Filtros

--- 09/main.js
+++ 09/main.js
@@ -2,6 +2,11 @@ Vue.filter('mayusculas', function(value) {
     return value.toUpperCase();
 });
 
+Vue.filter('formal', function(value, hombre) {
+    titulo = hombre ? 'Sr. ' : 'Sra. ';
+    return titulo + value.split(' ').reverse().join(' ');
+});
+
 new Vue({
     el: 'body',
     data: {
