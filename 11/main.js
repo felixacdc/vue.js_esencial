516b74b78e1750c42720d60bb0d054298762be37
felixacdc <pfmata01@gmail.com>
2017-02-20 22:10:06 -0600

    [modify] Aprende Vue.js esencial #11 - Data binding en formularios

--- 11/main.js
+++ 11/main.js
@@ -3,9 +3,12 @@ new Vue({
     data: {
         mensaje: 'Hola desde DOPodcast',
         chequeado: true,
-        radio: null
-    },
-    methods: {
-
+        radio: null,
+        seleccionado: null,
+        tareas: [
+            {titulo: 'Aprender Vue.js'},
+            {titulo: 'Aprender Angular.js'},
+            {titulo: 'Aprender Vanilla Js'}
+        ]
     }
 });
