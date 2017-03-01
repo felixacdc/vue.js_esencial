68aab98c7dac636db891c18d9963d288c37a63ee
felixacdc <pfmata01@gmail.com>
2017-02-28 22:09:28 -0600

    [modify] #13 - Vue & Firebase

---
+++ 13/main.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: 'body',
+    data: {
+        tareas: [
+            {titulo: 'Salir a correr', completado: false},
+            {titulo: 'Ir al gimnasio', completado: false},
+            {titulo: 'Limpiar el coche', completado: false},
+            {titulo: 'Hacer la compra', completado: false},
+            {titulo: 'Aprender VueJs & Firebase', completado: false}
+        ]
+    }
+});
