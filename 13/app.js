741f122882af4787a5a0ab574c6e9c3c1b8dfbcb
felixacdc <pfmata01@gmail.com>
2017-03-20 11:50:09 -0600

    [modify] 15 - add server

--- 13/app.js
+++ 13/app.js
@@ -2,6 +2,8 @@ var express = require('express'),
 	app = express(),
 	path = require('path');
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'))
 })
