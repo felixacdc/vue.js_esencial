321091ea1623061e6e417549b8f4e1ecd31b5d3d
felixacdc <pfmata01@gmail.com>
2017-03-22 22:05:11 -0600

    [modify] Aprende Vue.js esencial #17 - Advanced workflow ES6: let

--- 17/main.js
+++ 17/main.js
@@ -1,6 +1,6 @@
 function activar(orden) {
 	if (orden) {
-		var mensaje = "Lanzamiento en 3,2,1,0!";
+		let mensaje = "Lanzamiento en 3,2,1,0!";
 		console.log(mensaje);
 	} else {
 		console.log(mensaje);
