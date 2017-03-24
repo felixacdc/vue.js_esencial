0366eb30d85c628546a7e0112af12fb95b433ee3
felixacdc <pfmata01@gmail.com>
2017-03-24 10:31:10 -0600

    [modify] Aprende Vue.js esencial #19 - Advanced workflow ES6_ arrow functions

--- 19/main.js
+++ 19/main.js
@@ -1,7 +1,25 @@
 var dias = ['Lunes', 'Martes', 'Miercoles', 'Juevez', 'Viernes'];
 
-dias.forEach(function(dia) {
-	console.log(dia);
-});
+// dias.forEach(function(dia) {
+// 	console.log(dia);
+// });
 
-dias.forEach(dia => console.log(dia));
+// dias.forEach(dia => console.log(dia));
+
+// un parametro
+// let sumar = function(numero) {
+// 	return numero + 1;
+// };
+
+// let sumar = numero => numero + 1;
+// console.log(sumar(10));
+
+// Varios parametros
+// let multiplicar = function(numero1, numero2) {
+// 	return numero1 * numero2;
+// };
+
+// let multiplicar = (numero1, numero2) => numero1 * numero2;
+
+// console.log(multiplicar(10,10));
+
