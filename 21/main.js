2854598b275d3fd1af2f56b3a309300e78a2cf7f
felixacdc <pfmata01@gmail.com>
2017-03-24 14:56:16 -0600

    [modify] Aprende Vue.js esencial #21 - Advanced workflow ES6_ clases

--- 21/main.js
+++ 21/main.js
@@ -8,6 +8,32 @@ Persona.prototype.saludar = function() {
 	console.log('Hola soy ' + this.nombre + ' y vivo en ' + this.ciudad);
 };
 
-var juan = new Persona('Juan', 'Nuñez', 'Valencia');
-console.dir(juan);
-juan.saludar();
+// var juan = new Persona('Juan', 'Nuñez', 'Valencia');
+// console.dir(juan);
+// juan.saludar();
+
+class Humano {
+	constructor(nombre, apellido, ciudad) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.ciudad = ciudad;
+	}
+
+	saludar() {
+		console.log('Hola soy ' + this.nombre + ' y vivo en ' + this.ciudad);
+	}
+
+	get nombreCompleto() {
+		return this.nombre + " " + this.apellido;
+	}
+
+	set cambiarCiudad(ciudad) {
+		this.ciudad = ciudad;
+	}
+}
+
+let andres = new Humano('Andres', 'Nuñez', 'Valencia');
+console.dir(andres);
+console.log(andres.nombreCompleto)
+andres.cambiarCiudad = 'Ponferrada';
+console.dir(andres);
