6491fea828f15b4bf6976e02041f113f77be972a
felixacdc <pfmata01@gmail.com>
2017-03-24 10:11:29 -0600

    [add] Aprende Vue.js esencial #18 - Advanced workflow ES6_ const

---
+++ 18/main.js
@@ -0,0 +1,11 @@
+// var IVA = 21;
+// IVA = 22;
+// const IVA = 21;
+// IVA = 22;
+// console.log(IVA);
+
+var matriz = ['a', 'b', 'c', 'd'];
+const LAMATRIZ = matriz;
+// LAMATRIZ = ['a', 'b', 'c', 'd', 'e']; <--ERROR
+LAMATRIZ.push('e');
+console.log(LAMATRIZ);
