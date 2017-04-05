1f2363bbd5f2538474083a41469045ffe62dd44c
felixacdc <pfmata01@gmail.com>
2017-04-04 21:03:45 -0600

    [modify] Aprende Vue.js esencial #24 - Instalando y configurando Babel

--- vue-babel/src/main.js
+++ vue-babel/src/main.js
@@ -1,1 +1,14 @@
 let saludar = nombre => console.log(nombre);
+
+class Gato {
+    constructor(nombre = 'Guizmo') {
+        this.nombre = nombre;
+    }
+
+    curarse() {
+        return `${this.nombre} se esta curando`;
+    }
+}
+
+let Guizmo = new Gato();
+console.log(Guizmo.curarse());
