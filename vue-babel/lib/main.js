86231140d44cb048125af4c5a26d5eb01102e84b
felixacdc <pfmata01@gmail.com>
2017-04-07 14:43:42 -0600

    [add] Aprende Vue.js esencial #25 - Workflow con Babel y Gulp

---
+++ vue-babel/lib/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var saludar = function saludar(nombre) {
+    return console.log(nombre);
+};
+
+var Gato = function () {
+    function Gato() {
+        var nombre = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Guizmo';
+
+        _classCallCheck(this, Gato);
+
+        this.nombre = nombre;
+    }
+
+    _createClass(Gato, [{
+        key: 'curarse',
+        value: function curarse() {
+            return this.nombre + ' se esta curando';
+        }
+    }]);
+
+    return Gato;
+}();
+
+var Guizmo = new Gato();
+console.log(Guizmo.curarse());
